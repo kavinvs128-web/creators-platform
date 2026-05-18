9feddacdb5e026cec9b1740c634fbec929b970f0
kavin <kavinvenkatesan2008@gmail.com>
2026-05-18 12:19:27 +0530

    Add index to email field for faster login queries

--- creators-platform-main/server/models/User.js
+++ creators-platform-main/server/models/User.js
@@ -13,6 +13,7 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: [true, 'Email is required'],
       unique: true,
+      index: true,
       lowercase: true,
       trim: true,
       match: [
